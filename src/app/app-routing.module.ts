e471dcd7bc593ecdc2df5507b58370f2c7e159fd
Oleh <oleg_bob@ukr.net>
2023-01-06 17:28:11 +0200

    base structure & routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./features/registration/registration.module')
+        .then(m => m.RegistrationModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./features/login/login.module')
+        .then(m => m.LoginModule)
+    ,
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module')
+        .then(m => m.HomeModule)
+    ,
+  },
+  {
+    path: 'contacts',
+    loadChildren: () =>
+      import('./features/contacts/contacts.module')
+        .then(m => m.ContactsModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./features/products/products.module')
+        .then(m => m.ProductsModule)
+  },
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: APP_BASE_HREF, useValue: '/test-app'}],
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
